ecfdad9cc3ea136f69ba7cf68f1443fd1f78d696
tomimandalap <tommymandalaputra@gmail.com>
2025-03-05 09:42:44 +0700

    feat: setup application

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,44 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  {
+    rules: {
+      'indent': ['error', 2],
+      'semi': ['error', 'never'],
+      'quotes': ['error', 'single'],
+      'quote-props': ['error', 'consistent-as-needed'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'block-spacing': ['error', 'always'],
+      'arrow-parens': ['error', 'always'],
+      'max-len': [
+        'error',
+        { code: 150, tabWidth: 2, comments: 150 },
+      ],
+    },
+  },
+  {
+    ignores: [
+      '*.md',
+      '.next',
+      '.npm/**/*',
+      '.vscode/*.json',
+      'src/components/ui/*',
+      'commitlint.config.mjs',
+      'postcss.config.mjs',
+      'tailwind.config.ts',
+      'tsconfig.json',
+    ],
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+]
 
-export default eslintConfig;
+export default eslintConfig
