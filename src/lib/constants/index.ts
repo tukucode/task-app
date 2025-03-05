0db46f45579923c7f9d732e7c6eda73d8bb871fd
tomimandalap <tommymandalaputra@gmail.com>
2025-03-05 12:58:00 +0700

    feat: setup font roboto and define environment meta app

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'APP NAME'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'DESCRIPTION'
+export const PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
